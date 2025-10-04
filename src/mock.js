3a08d8d35da3430b22df9e452d59bf0f57b5420f
dk00 <dk@csie.org>
2025-10-04 08:46:11 +0800

    chore: mock data

---
+++ src/mock.js
@@ -0,0 +1,20 @@
+const mock = () => [
+  {
+    date: '2025-10-25T06:50:00Z',
+    location: 'KHH',
+    duration: 245,
+    destination: 'KIX',
+    tags: ['departure'],
+    flight: 'MM 32',
+  },
+  {
+    date: '2025-10-30T07:45:00Z',
+    location: 'KMQ',
+    duration: 215,
+    destination: 'TPE',
+    tags: ['return'],
+    flight: 'IT 253',
+  },
+]
+
+export default mock
